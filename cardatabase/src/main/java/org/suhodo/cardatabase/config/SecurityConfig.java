a6b4f161b2b126afad0ec5ed61615596768a04a4
SJF02 <suhodo1126@gmail.com>
2025-06-24 17:02:55 +0900

    SecurityConfig 설정 중...

--- cardatabase/src/main/java/org/suhodo/cardatabase/config/SecurityConfig.java
+++ cardatabase/src/main/java/org/suhodo/cardatabase/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package org.suhodo.cardatabase.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.suhodo.cardatabase.service.UserDetailsServiceImpl;
+
+import lombok.RequiredArgsConstructor;
 
 /*
  * 스프링 시큐리티 설정
@@ -13,7 +17,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     // BCrypt 해싱 알고리즘 객체(password -> hashcode로 변환)
     @Bean
