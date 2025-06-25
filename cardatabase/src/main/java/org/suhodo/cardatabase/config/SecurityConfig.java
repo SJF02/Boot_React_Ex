43d2a3072a1b944f8a9f85d23e9de6e0f6948c11
SJF02 <suhodo1126@gmail.com>
2025-06-25 10:38:17 +0900

    AuthenticationManagerBuilder 직접 등록

--- cardatabase/src/main/java/org/suhodo/cardatabase/config/SecurityConfig.java
+++ cardatabase/src/main/java/org/suhodo/cardatabase/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package org.suhodo.cardatabase.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.suhodo.cardatabase.service.UserDetailsServiceImpl;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import lombok.extern.log4j.Log4j2;
 
 /*
@@ -21,22 +26,26 @@ import lombok.extern.log4j.Log4j2;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
+   
     private final UserDetailsServiceImpl userDetailsService;
 
     // 시큐리티 인증과정에 필요한 AuthenticationManagerBuilder 객체에
     // DB의 사용자 정보를 반환하는 역할을 하는 userDetailsService를 등록
     // 패스워드 암호화 시 BCryptPasswordEncoder를 사용하겠다.
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+    private final AuthenticationManagerBuilder auth;
+
+    @PostConstruct
+    public AuthenticationManagerBuilder configureGlobal() throws Exception {
         log.info("configureGlobal................");
-        
+
         auth.userDetailsService(userDetailsService)
-        .passwordEncoder(new BCryptPasswordEncoder());
+                .passwordEncoder(new BCryptPasswordEncoder());
+        return auth;
     }
 
     // BCrypt 해싱 알고리즘 객체(password -> hashcode로 변환)
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
