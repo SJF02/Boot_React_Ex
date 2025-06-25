3aafc6aea814ff0da73a10edef74a0312bc9e727
SJF02 <suhodo1126@gmail.com>
2025-06-25 15:24:09 +0900

    ROLE 권한에 따른 메서드 호출 권한

--- cardatabase/src/main/java/org/suhodo/cardatabase/filter/AuthenticationFilter.java
+++ cardatabase/src/main/java/org/suhodo/cardatabase/filter/AuthenticationFilter.java
@@ -1,14 +1,21 @@
 package org.suhodo.cardatabase.filter;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.suhodo.cardatabase.domain.AppUser;
+import org.suhodo.cardatabase.repository.AppUserRepository;
 import org.suhodo.cardatabase.service.JwtService;
 
 import jakarta.servlet.FilterChain;
@@ -24,6 +31,7 @@ import lombok.extern.log4j.Log4j2;
 public class AuthenticationFilter extends OncePerRequestFilter{
 
     private final JwtService jwtService;
+    private final AppUserRepository appUserRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -34,11 +42,20 @@ public class AuthenticationFilter extends OncePerRequestFilter{
         if(jws != null){        // 토큰이 존재한다면
             // 토큰 검증 및 사용자 반환
             String username = jwtService.getAuthUser(request);
-            
+
+            Optional<AppUser> result = appUserRepository.findByUsername(username);
+
+            AppUser appUser = result.get();
+
+            // 권한 리스트 생성 (예: "ROLE_USER" 등)
+            List<GrantedAuthority> authorities = List.of(
+                new SimpleGrantedAuthority("ROLE_" + appUser.getRole())
+            );
+
             // 인증하기
-            Authentication authEntication = 
+            Authentication authEntication =
                 new UsernamePasswordAuthenticationToken(
-                        username, null, java.util.Collections.emptyList());
+                    username, null, authorities);
 
             // 스프링 시큐리티에 인증정보를 등록
             SecurityContextHolder.getContext().setAuthentication(authEntication);
