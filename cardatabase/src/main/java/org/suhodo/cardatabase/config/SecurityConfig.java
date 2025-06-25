cc857a4c7f72719c65be0ed4cd499f8a192155bd
SJF02 <suhodo1126@gmail.com>
2025-06-25 12:05:06 +0900

    스프링 시큐리티 filterChain 설정

--- cardatabase/src/main/java/org/suhodo/cardatabase/config/SecurityConfig.java
+++ cardatabase/src/main/java/org/suhodo/cardatabase/config/SecurityConfig.java
@@ -3,12 +3,16 @@ package org.suhodo.cardatabase.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.suhodo.cardatabase.service.UserDetailsServiceImpl;
 
 import jakarta.annotation.PostConstruct;
@@ -51,5 +55,26 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // LoginController에서 사용할 AuthenticationManager객체를 Bean으로 등록
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+
+    // 스프링 시큐리티에서 어떤 경로는 보호/비보호 결정
+    // 보안 적용/비적용 결정
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf((csrf)->csrf.disable())
+            .sessionManagement((sessionManagement)->sessionManagement.
+                    sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests
+                    .requestMatchers(HttpMethod.POST, "/login").permitAll().
+                    anyRequest().authenticated());
+
+        return http.build();
     }
 }
